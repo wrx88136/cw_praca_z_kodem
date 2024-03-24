c49de80ce0d4905520ad20385ea54f1a3b3b25b7
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-03-24 17:20:45 +0100

    Add Zadanie 5.pdf and modyfi app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def index():
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def hello(Radoslaw=None):
+    return render_template('hello.html', name=Radoslaw)
